0a5877d391520e2eda71747a194ea496a4463054
Rafael Correia <raffcorreia@gmail.com>
2021-03-02 08:58:22 -0500

    add http call to retrieve XSS from the backend and confgure proxy

--- frontend/src/app/services/xss.service.ts
+++ frontend/src/app/services/xss.service.ts
@@ -1,31 +1,19 @@
 import { Injectable } from '@angular/core';
 import {Xss} from './xss';
 import {Observable, of} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class XssService {
-  XSS: Xss[];
 
-  constructor() {
-    this.XSS = [
-      { id: 11, text: 'Dr Nice' },
-      { id: 12, text: 'Narco' },
-      { id: 13, text: 'Bombasto' },
-      { id: 14, text: 'Celeritas' },
-      { id: 15, text: 'Magneta' },
-      { id: 16, text: 'RubberMan' },
-      { id: 17, text: 'Dynama' },
-      { id: 18, text: 'Dr IQ' },
-      { id: 19, text: 'Magma' },
-      { id: 20, text: 'Tornado' }
-    ];
+  private getXSSUrl = '/getXSS';
+
+  constructor(private http: HttpClient) {
   }
 
-
   getXssList(): Observable<Xss[]> {
-    const xss = of(this.XSS);
-    return xss;
+    return this.http.get<Xss[]>(this.getXSSUrl);
   }
 }
