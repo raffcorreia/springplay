57d7fc40e656e67432c1c0c746efb27f60ba78b2
Rafael Correia <raffcorreia@gmail.com>
2021-03-17 23:27:44 -0400

    add rout

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {XssExperienceComponent} from './xss-experience/xss-experience.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'xss-experience',
+    pathMatch: 'full'
+  },
+  {
+    path: 'xss-experience',
+    component: XssExperienceComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
