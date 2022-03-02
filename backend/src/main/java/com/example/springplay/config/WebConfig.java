52e5e2e15333397b6b9956c62d19adb6489a655f
Rafael Correia <raffcorreia@gmail.com>
2022-03-02 00:30:03 -0500

    Fix spring webcomponent redirection to accomodate the Angular and a JS applications, update build folders, and and add tests

--- backend/src/main/java/com/example/springplay/config/WebConfig.java
+++ backend/src/main/java/com/example/springplay/config/WebConfig.java
@@ -4,20 +4,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
 
+/**
+ * With this WebMvcConfigurer there are multiple types of redirection for your tasting
+ *
+ * addViewControllers -> adding redirect view and forwarding the controller
+ *
+ * addResourceHandlers -> Custom handle all reuqest to a folder
+ *
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addViewControllers (ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/js", "/js/");
+        registry.addRedirectViewController("/", "/angular/index.html");
+        registry.addRedirectViewController("/angular", "/angular/index.html");
+        registry.addRedirectViewController("/angular/", "/angular/index.html");
+        registry.addViewController("/angular/{path:[^.]*}").setViewName("forward:/angular/index.html");
 
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
-                .addResourceHandler("/js")
-                .addResourceLocations("classpath:/static/js/index.html")
+                .addResourceHandler("/js/", "/js/**")
+                .addResourceLocations("classpath:/static/js/")
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver() {
                     @Override
