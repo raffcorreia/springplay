68ed2fce08030ea4fdfbb5b58c1859231e17241b
Rafael Correia <raffcorreia@gmail.com>
2022-03-06 00:04:31 -0500

    fixed issue with local proxy and add Authentication token for dev environment

--- frontend/src/app/config/APIInterceptor.ts
+++ frontend/src/app/config/APIInterceptor.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {Injectable, isDevMode} from '@angular/core';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
@@ -8,8 +8,15 @@ export class APIInterceptor implements HttpInterceptor {
     private readonly BASE_URL: string = '/api/experience';
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("Intercepting...")
+        let isRunningAsDev = isDevMode();
+        console.log("Intercepting( isDevMode = " + isRunningAsDev + " )...")
+
         const apiReq = req.clone({url: this.BASE_URL + `${req.url}`});
+
+        if(isRunningAsDev) {
+            apiReq.headers.append("Basic", "YWRtaW46YWRtaW4=");
+        }
+
         return next.handle(apiReq);
     }
 }
