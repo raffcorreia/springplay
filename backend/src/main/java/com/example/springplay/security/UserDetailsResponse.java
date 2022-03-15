31ac6145702ea44858eb56927795c0f51fcb2017
Rafael Correia <raffcorreia@gmail.com>
2022-03-15 13:41:16 -0400

    Create a security controller to provide FE with user information

---
+++ backend/src/main/java/com/example/springplay/security/UserDetailsResponse.java
@@ -0,0 +1,13 @@
+package com.example.springplay.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UserDetailsResponse {
+
+    private String name;
+}
