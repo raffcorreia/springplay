09c7c11156d446546e1b514c2f9af0a5dfe20d82
Rafael Correia <raffcorreia@gmail.com>
2022-03-16 09:14:10 -0400

    remove unecessary logs

--- frontend/src/app/services/security.service.ts
+++ frontend/src/app/services/security.service.ts
@@ -15,13 +15,7 @@ export class SecurityService {
   constructor(private http: HttpClient) { }
 
   getUserDetails(): Promise<UserDetails> {
-    console.log('getUserDetails(...)');
-    console.log(this.userDetails);
-    console.log(!this.userDetails);
-
     if(!this.userDetails) {
-      console.log('It is going to retrieve User Details...');
-
       this.userDetails = this.http.get<UserDetails>(this.GET_USER_DETAILS).pipe(share()).toPromise();
     }
 
