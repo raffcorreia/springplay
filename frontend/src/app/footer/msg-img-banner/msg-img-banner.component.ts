b20b1f04c69662c26c8c140c0d8c0a6af31918d5
Rafael Correia <raffcorreia@gmail.com>
2022-03-16 16:03:03 -0400

    update msg-img-banner component

--- frontend/src/app/footer/msg-img-banner/msg-img-banner.component.ts
+++ frontend/src/app/footer/msg-img-banner/msg-img-banner.component.ts
@@ -10,13 +10,15 @@ import {BannerMessage} from "../banner.message";
 export class MsgImgBannerComponent implements OnInit {
   public static TYPE: BannerType = BannerType.MSG_IMG;
   message: BannerMessage;
+  base64Img: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public setMessage(message: BannerMessage) {
+  public setMessage(message: any) {
     this.message = message;
+    this.base64Img = atob(message.img);
   }
 }
