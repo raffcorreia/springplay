3ddec28b5491b9eb9bcd288db5069ad6c6e520eb
Rafael Correia <raffcorreia@gmail.com>
2022-05-13 00:48:44 -0400

    add record object with validations

--- backend/src/main/java/com/example/springplay/validation/Contact.java
+++ backend/src/main/java/com/example/springplay/validation/Contact.java
@@ -1,23 +1,18 @@
 package com.example.springplay.validation;
 
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Getter
-@Setter
-public class Contact {
+public record Contact(
     @NotNull
     @NotBlank
-    private String mediaName;
-    private String user;
-    private String url;
+    String mediaName,
+    String user,
+    String url,
 
     @Valid
-    private List<@NotBlank String> friends;
+    List<@NotBlank String> friends
+) {
 }
--- backend/src/main/java/com/example/springplay/validation/ValidationRequest.java
+++ backend/src/main/java/com/example/springplay/validation/ValidationRequest.java
@@ -1,53 +1,49 @@
 package com.example.springplay.validation;
 
 import com.example.springplay.validation.annotation.NoAs;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.util.Date;
 import java.util.List;
 
-@Getter
-@Setter
-public class ValidationRequest {
-
+public record ValidationRequest(
     @NotBlank(message = "Name can't be blank")
-    private String name;
+    String name,
 
     @NotNull(message = "Role can't be null")
-    private String role;
+    String role,
 
     @Email(message = "Email is not valid")
-    private String email;
+    String email,
 
     @Min(value = 18, message = "Age should not be less than 18")
-    private Integer age;
+    Integer age,
 
     @Size(min = 10, max = 200, message = "Message should be between 10 and 200 characters")
-    private String message;
+    String message,
 
     @AssertTrue(message = "This resource is exclusive for FTEs")
-    private boolean fullTimeEmloyee;
+    boolean fullTimeEmloyee,
 
     @Min(value = 100_000, message = "You have to make at lease $100.000 to access this resource")
     @Max(value = 500_000, message = "You can't make more then $500.000 to access this resource")
-    private Integer wage;
+    Integer wage,
 
     @Past(message = "Birth date must be in the past")
-    private Date birthDate;
+    Date birthDate,
 
     @Future(message = "Expire date must be in the future")
-    private Date expireDate;
+    Date expireDate,
 
     @NotNull(message = "Project names can't be null")
-    private List<String> projectNames;
+    List<String> projectNames,
 
     @Valid
     @NotNull(message = "Contacts can't be null")
-    private List<@NotNull Contact> contacts;
+    List<@NotNull Contact> contacts,
 
     @NoAs
-    private String noLetterAmessage;
+    String noLetterAmessage
+) {
 }
